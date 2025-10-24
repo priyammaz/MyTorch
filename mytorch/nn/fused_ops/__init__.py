7172eb571e3253ddc702b3dd1886724013d6f8e5
priyammaz <priyammazumdar1@gmail.com>
2025-10-24 14:16:35 -0500

    added in fused ops to conv1/2d and minor fixes

--- mytorch/nn/fused_ops/__init__.py
+++ mytorch/nn/fused_ops/__init__.py
@@ -2,3 +2,5 @@ from .softmax import fused_softmax_forward, fused_softmax_backward
 from .layernorm import fused_layernorm_forward, fused_layernorm_backward
 from .cross_entropy import fused_cross_entropy_forward, fused_cross_entropy_backward
 from .flash_attention import fused_sdpa_forward, fused_sdpa_backward
+from .conv import fused_conv2d_forward, fused_conv2d_backward, \
+    fused_conv1d_forward, fused_conv1d_backward
