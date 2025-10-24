a2b7dba3a7540b247d06cd7660ca7c31b70f0c14
priyammaz <priyammazumdar1@gmail.com>
2025-10-24 14:19:31 -0500

    tiny resnet example

--- training_scripts/train_conv2d_cifar10_classifier.py
+++ training_scripts/train_conv2d_cifar10_classifier.py
@@ -12,49 +12,77 @@ from torchvision.datasets import CIFAR10
 def main(args):
 
     ### Prep Model ###
-    class MyTorchCIFAR10(nn.Module):
 
-        def __init__(self):
-            super().__init__()
-
-            self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3,
-                                   stride=2, padding=1)
-            self.bn1 = nn.BatchNorm2d(32)
-            self.drop1 = nn.Dropout(0.1)
-
-            self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, 
-                                   stride=2, padding=1)
-            self.bn2 = nn.BatchNorm2d(64)
-            self.drop2 = nn.Dropout(0.1)
+    class ResidualBlock(nn.Module):
+        def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+            super(ResidualBlock, self).__init__()
+            self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+            self.bn1 = nn.BatchNorm2d(out_channels)
+            self.relu = nn.ReLU()
+            self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+            self.bn2 = nn.BatchNorm2d(out_channels)
             
-            self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3,
-                                   stride=2, padding=1)
-            self.bn3 = nn.BatchNorm2d(128)
-            self.drop3 = nn.Dropout(0.1)
-            
-            self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3,
-                                   stride=2, padding=1)
-            self.bn4 = nn.BatchNorm2d(256)
-            self.drop4 = nn.Dropout(0.1)
-            
-            self.conv5 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3,
-                                   stride=1, padding=1)
-            self.bn5 = nn.BatchNorm2d(256)
-            self.drop5 = nn.Dropout(0.1)
-
-            self.activation = nn.ReLU()
-            self.proj = nn.Linear(256*2*2, 10)
+            # Downsample path if needed (for matching dimensions)
+            self.downsample = downsample
 
         def forward(self, x):
+            identity = x  # shortcut connection
             
-            x = self.drop1(self.activation(self.bn1(self.conv1(x))))
-            x = self.drop1(self.activation(self.bn2(self.conv2(x))))
-            x = self.drop1(self.activation(self.bn3(self.conv3(x))))
-            x = self.drop1(self.activation(self.bn4(self.conv4(x))))
-            x = self.drop1(self.activation(self.bn5(self.conv5(x))))
-     
-            x = x.reshape(-1, 256*2*2)
-            x = self.proj(x)
+            out = self.conv1(x)
+            out = self.bn1(out)
+            out = self.relu(out)
+            
+            out = self.conv2(out)
+            out = self.bn2(out)
+            
+            if self.downsample is not None:
+                identity = self.downsample(x)  # match dimensions
+            
+            out = out + identity  # residual connection
+            out = self.relu(out)
+            
+            return out
+
+    class MyTorchCIFAR10(nn.Module):
+        def __init__(self):
+            super(MyTorchCIFAR10, self).__init__()
+            
+            self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=2, padding=1)
+            self.bn1 = nn.BatchNorm2d(16)
+            self.relu = nn.ReLU()
+            
+            # Adding residual blocks
+            self.layer1 = self._make_layer(16, 32, stride=2)
+            self.layer2 = self._make_layer(32, 64, stride=2)
+            self.layer3 = self._make_layer(64, 128, stride=2)
+            self.layer4 = self._make_layer(128, 128, stride=1)
+            
+            self.avgpool = nn.AdaptiveAvgPool2d(1)  # Global average pooling
+            self.fc = nn.Linear(512, 10)  # Final fully connected layer for 10 classes
+        
+        def _make_layer(self, in_channels, out_channels, stride):
+            downsample = None
+            if stride != 1 or in_channels != out_channels:
+                downsample = nn.Sequential(
+                    nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                    nn.BatchNorm2d(out_channels)
+                )
+            
+            layers = []
+            layers.append(ResidualBlock(in_channels, out_channels, stride, downsample))
+            layers.append(ResidualBlock(out_channels, out_channels))  # Second block
+            
+            return nn.Sequential(*layers)
+
+        def forward(self, x):
+            x = self.relu(self.bn1(self.conv1(x)))  # Initial convolution and batch norm
+            x = self.layer1(x)  # First residual block layer
+            x = self.layer2(x)  # Second residual block layer
+            x = self.layer3(x)  # Third residual block layer
+            x = self.layer4(x)  # Fourth residual block layer
+            x = x.reshape(-1, 128*2*2)
+            x = self.fc(x)  # Final classification layer
+            
             return x
         
     model = MyTorchCIFAR10()
@@ -104,7 +132,7 @@ def main(args):
 
             ### Compute Loss ###
             loss = loss_fn(pred, labels)
-     
+  
             ### Compute Accuracy ###
             predicted = pred.argmax(dim=-1)
             accuracy = (predicted == labels).sum() / len(predicted)
