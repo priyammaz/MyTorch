f0b6a9ee8a7a14d276fd03130eaad758ed28610d
priyammaz <priyammazumdar1@gmail.com>
2025-11-07 06:52:44 -0600

    cleanup

--- models/gpt2.py
+++ models/gpt2.py
@@ -218,7 +218,9 @@ class GPT2(nn.Module):
             for _ in range(config.num_blocks)
         ])
 
-        self.final_layer_norm = nn.LayerNorm(config.embed_dim)
+        self.final_layer_norm = nn.LayerNorm(config.embed_dim, 
+                                             auto=config.use_full_auto, 
+                                             fused=config.use_fused_ops)
         self.lm_head = nn.Linear(config.embed_dim, 
                                  config.vocab_size, 
                                  bias=config.use_bias,
