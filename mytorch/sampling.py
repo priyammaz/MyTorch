1c154284744814467127fcf5ada74853e8395431
priyammaz <priyammazumdar1@gmail.com>
2025-11-07 20:58:25 -0600

    fixed a bug with fp16 that makes the last element of the CDF not exactly 1

--- mytorch/sampling.py
+++ mytorch/sampling.py
@@ -7,7 +7,17 @@ def multinomial(probs, num_samples=1):
     probs: shape (..., n_classes)
     returns: shape (..., num_samples)
     """
+    
+    ### Cast to float32 otherwise there will be precision errors! ###
+    probs = probs.astype("float32")
     probs = probs / sum(probs, dim=-1, keepdims=True)
     cdf = cumsum(probs, dim=-1)
+
+    ### CDF must add to 1, otherwise it doesnt make sense. The reason for the 
+    ### float32 cast was if in float16 it can give values like 0.99995 which is 
+    ### undesireable and leads to incorrect indexes !
+    assert cdf[..., -1] == 1.0
+    
     u = rand((*probs.shape[:-1], num_samples), device=probs.device).unsqueeze(-1)
-    return sum(u > cdf.unsqueeze(-2), dim=-1)
+    sampled = sum(u > cdf.unsqueeze(-2), dim=-1)
+    return sampled
