4dc47324767b1295336908c28c4bfcbe39428158
priyammaz <priyammazumdar1@gmail.com>
2025-11-21 13:24:52 -0600

    uncommented

--- nanochat_trainer/run.sh
+++ nanochat_trainer/run.sh
@@ -35,41 +35,41 @@ mkdir -p "$DOWNLOAD_PATH/tokenized"
 
 export HF_HOME=$HF_CACHE_DIR
 
-# ### DOWNLOAD SLICE OF FINEWEB ###
-# ### With the default settings this will download 20 parquet files from 100BT split
-# ### and will save a final 29 parquet files (about 55GB of data!)
-# python -m nanochat_trainer.scripts.download_fineweb_edu \
-#     --path_to_save "$DOWNLOAD_PATH/raw_text" \
-#     --num_workers $NUM_WORKERS \
-#     --chinchilla_ratio $CHINCHILLA_RATIO \
-#     --vocab_size $VOCAB_SIZE \
-#     --context_length $CONTEXT_LENGTH \
-#     --num_blocks $NUM_BLOCKS \
-#     --embed_dim $EMBED_DIM \
-#     --num_q_heads $NUM_Q_HEADS \
-#     --num_kv_heads $NUM_KV_HEADS \
-#     --mlp_ratio $MLP_RATIO
+### DOWNLOAD SLICE OF FINEWEB ###
+### With the default settings this will download 20 parquet files from 100BT split
+### and will save a final 29 parquet files (about 55GB of data!)
+python -m nanochat_trainer.scripts.download_fineweb_edu \
+    --path_to_save "$DOWNLOAD_PATH/raw_text" \
+    --num_workers $NUM_WORKERS \
+    --chinchilla_ratio $CHINCHILLA_RATIO \
+    --vocab_size $VOCAB_SIZE \
+    --context_length $CONTEXT_LENGTH \
+    --num_blocks $NUM_BLOCKS \
+    --embed_dim $EMBED_DIM \
+    --num_q_heads $NUM_Q_HEADS \
+    --num_kv_heads $NUM_KV_HEADS \
+    --mlp_ratio $MLP_RATIO
 
-# ### TRAIN TOKENIZER ON FINEWEB ###
-# python -m nanochat_trainer.scripts.train_tokenizer \
-#     --comparison_tokenizer "gpt2" \
-#     --path_to_dataset "$DOWNLOAD_PATH/raw_text" \
-#     --vocab_size 65536 \
-#     --path_to_save_tokenizer $PATH_TO_SAVE_TOKENIZER
+### TRAIN TOKENIZER ON FINEWEB ###
+python -m nanochat_trainer.scripts.train_tokenizer \
+    --comparison_tokenizer "gpt2" \
+    --path_to_dataset "$DOWNLOAD_PATH/raw_text" \
+    --vocab_size 65536 \
+    --path_to_save_tokenizer $PATH_TO_SAVE_TOKENIZER
 
-# ### Tokenize and Save Dataset 
-# python -m nanochat_trainer.scripts.prepare_fineweb \
-#     --path_to_tokenizer $PATH_TO_SAVE_TOKENIZER \
-#     --path_to_data "$DOWNLOAD_PATH/raw_text" \
-#     --path_to_save "$DOWNLOAD_PATH/tokenized" \
-#     --num_workers $NUM_WORKERS \
-#     --max_seq_len $CONTEXT_LENGTH
+### Tokenize and Save Dataset 
+python -m nanochat_trainer.scripts.prepare_fineweb \
+    --path_to_tokenizer $PATH_TO_SAVE_TOKENIZER \
+    --path_to_data "$DOWNLOAD_PATH/raw_text" \
+    --path_to_save "$DOWNLOAD_PATH/tokenized" \
+    --num_workers $NUM_WORKERS \
+    --max_seq_len $CONTEXT_LENGTH
 
-# ### Delete Everything in Cache, Dont Need it Anymore ###
-# rm -r $HF_CACHE_DIR/*
+### Delete Everything in Cache, Dont Need it Anymore ###
+rm -r $HF_CACHE_DIR/*
 
-# ### Delete the downloaded raw data, Dont need it Anymore ###
-# rm -r "$DOWNLOAD_PATH/raw_text"
+### Delete the downloaded raw data, Dont need it Anymore ###
+rm -r "$DOWNLOAD_PATH/raw_text"
 
 # ===================================================================
 # PRETRAINING (the expensive part)
